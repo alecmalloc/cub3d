3d39d2e2d445cc90e6b17a7239355ab64b4b3508
alecmalloc <aeastman@student.42berlin.de>
2024-03-14 13:55:10 +0100

    added window.c to make

--- src/mlx/window.c
+++ src/mlx/window.c
@@ -6,7 +6,7 @@
 /*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 13:48:29 by aeastman          #+#    #+#             */
-/*   Updated: 2024/03/14 13:48:58 by aeastman         ###   ########.fr       */
+/*   Updated: 2024/03/14 13:53:22 by aeastman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	mlx_handler(t_cubed	*cubed)
 {
-	cubed->ptr_mlx = mlx_init(500, 500, "cub3d", false);
+	cubed->ptr_mlx = mlx_init(1000, 500, "cub3d", false);
 	if (!(cubed->ptr_mlx))
 		return (EXIT_FAILURE);
 	mlx_set_window_pos(cubed->ptr_mlx, 20, 30);
