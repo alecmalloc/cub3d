f49a6ec6e3d886ecf687c9a89f71f864f9682e02
alecmalloc <aeastman@student.42berlin.de>
2024-03-14 13:52:26 +0100

    comments

--- src/main.c
+++ src/main.c
@@ -6,12 +6,13 @@
 /*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 09:48:55 by aeastman          #+#    #+#             */
-/*   Updated: 2024/03/14 13:48:56 by aeastman         ###   ########.fr       */
+/*   Updated: 2024/03/14 13:52:18 by aeastman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cubed.h"
 
+// load up main struct and initalize the mlx window
 int	main(void)
 {
 	t_cubed	cubed;
