10dd7c618fed3536b936f6ca2bed517ca704adc7
alecmalloc <aeastman@student.42berlin.de>
2024-03-21 15:13:25 +0100

    put inits into initer

--- src/init/initer.c
+++ src/init/initer.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   initer.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aeastman <aeastman@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/21 12:58:14 by aeastman          #+#    #+#             */
+/*   Updated: 2024/03/21 15:12:46 by aeastman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cubed.h"
 
 static int	init_cubed(t_cubed **cubed)
@@ -9,9 +21,33 @@ static int	init_cubed(t_cubed **cubed)
 	return (0);
 }
 
+static int	init_f_imgs(t_cubed **cubed)
+{
+	t_f_img	*floor;
+	t_f_img	*ceiling;
+
+	floor = malloc(sizeof(t_f_img));
+	ceiling = malloc(sizeof(t_f_img));
+	if (!floor || !ceiling)
+		return (MALL_ERR);
+	floor->image = NULL;
+	floor->r = 0;
+	floor->g = 0;
+	floor->b = 0;
+	ceiling->image = NULL;
+	ceiling->r = 0;
+	ceiling->g = 0;
+	ceiling->b = 0;
+	(*cubed)->ceiling = ceiling;
+	(*cubed)->floor = floor;
+	return (0);
+}
+
 int	init_all(t_cubed **cubed)
 {
 	if (init_cubed(cubed))
+		return (MALL_ERR);
+	if (init_f_imgs(cubed))
 		return (MALL_ERR);
 	if (init_parser(&(*cubed)->parser))
 		return (MALL_ERR);
