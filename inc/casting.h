456235a00fa6978b7809384379d130841372c5de
alecmalloc <aeastman@student.42berlin.de>
2024-03-30 18:27:35 +0100

    added struct for each ray

--- inc/casting.h
+++ inc/casting.h
@@ -1,6 +1,16 @@
 #ifndef CASTING_H
 # define CASTING_H
 
+typedef struct ray {
+	int step_x;
+	int	step_y;
+	double org_x;
+	double org_y;
+	double angle;
+	double ray_len_x;
+	double ray_len_y;
+} t_ray;
+
 typedef struct casting {
 	double	resolution_w;
 } t_casting;
--- src/casting/casting.c
+++ src/casting/casting.c
@@ -1,9 +1,19 @@
 #include "cubed.h"
 
-void	set_steps_x_y(int *step_x, int *step_y, double angle)
+void	set_steps_x_y(t_ray *ray)
 {
-	*step_x = 0;
-	*step_y = 0;
+	if (ray->angle > 90 && ray->angle < 270)
+		ray->step_x = -1;
+	if (ray->angle > 270 || ray->angle < 90)
+		ray->step_x = 1;
+	if (ray->angle > 180 && ray->angle < 360)
+		ray->step_y = -1;
+	if (ray->angle < 180 && ray->angle > 0)
+		ray->step_y = 1;
+}
+
+void	calc_init_distance(t_ray *ray)
+{
 	if (angle > 90 && angle < 270)
 		*step_x = -1;
 	if (angle > 270 || angle < 90)
@@ -14,22 +24,34 @@ void	set_steps_x_y(int *step_x, int *step_y, double angle)
 		*step_y = 1;
 }
 
-void	ray_calc_steps(double pos_x, double pos_y, double angle)
+void	ray_calc_steps(t_ray *ray)
 {
-	int step_x;
-	int	step_y;
-	(void)pos_x;
-	(void)pos_y;
-
-	set_steps_x_y(&step_x, &step_y, angle);
-	printf("stepx: %d stepy: %d\n", step_x, step_y);
+	set_steps_x_y(ray);
+	calc_init_distance(ray);
 }
 
-void	casting(t_cubed *cubed)
+void	*init_ray(t_ray *ray, double angle, double org_x, double org_y)
 {
+	ray = malloc(sizeof(ray));
+	if (!ray)
+		return ((void *)1);
+	ray->step_x = 0;
+	ray->step_y = 0;
+	ray->org_x = 0;
+	ray->org_y = 0;
+	ray->angle = angle;
+	ray->ray_len_x = 0;
+	ray->ray_len_y = 0;
+	return (0);
+}
+
+void	*casting(t_cubed *cubed)
+{
+	t_ray *ray;
 	double angle;
 	(void)cubed;
 
-	angle = 360;
-	ray_calc_steps(3.5, 3.5, angle);
+	if (init_ray(ray, 180, 3.5, 3.5) == 0)
+		return (MALL_ERR);
+	ray_calc_steps(ray);
 }
