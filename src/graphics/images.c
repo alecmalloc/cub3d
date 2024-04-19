8400ebe17ddc434549d0a7db426ef65cad347da9
mo <moba42@proton.me>
2024-04-19 18:38:19 +0200

    test

--- src/graphics/images.c
+++ src/graphics/images.c
@@ -4,6 +4,7 @@
 int	make_image(t_cubed *master)
 {
 	mlx_image_t	*new;
+//	xpm_t		*screen;
 
 	new = mlx_new_image(master->mlx_inst, WIDTH, HIGHT);
 	if (!new)
@@ -13,7 +14,9 @@ int	make_image(t_cubed *master)
 	load_minimap(new, master);
 	if (master->img)
 		mlx_delete_image(master->mlx_inst, master->img);
-	master->img = new;
+//	screen = mlx_load_xpm42("./textures/screen.xpm42");
+//	new = mlx_texture_to_image(master->mlx_inst, &screen->texture);
+//	master->img = new;
 	if (mlx_image_to_window(master->mlx_inst, new, 0, 0) < 0)
 		return (MLX_IMG_ERR);
 	return (0);
